0b3b984ce692d941f564733ab7c43f8e5e74b117
Venky Venkatakrishnan <venkyul@gmail.com>
2018-09-23 09:35:50 -0500

    Fixed date refresh issue

--- Temporal2/AppDelegate.swift
+++ Temporal2/AppDelegate.swift
@@ -11,9 +11,10 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-  
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         
+      
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
